b60645b8425e8f0e35324578031ca292080eb124
Steve Hechio <stevehechio@Steves-iMac.local>
2021-09-02 16:34:12 +0300

    apollo done

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.apollographql.apollo").version("2.5.5")
+}
+
+android {
+    compileSdkVersion(30)
+    buildToolsVersion("29.0.3")
+
+    defaultConfig {
+        applicationId = "com.example.rocketreserver"
+        minSdkVersion(23)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("androidx.appcompat:appcompat:1.3.1")
+    implementation("androidx.core:core-ktx:1.6.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("io.coil-kt:coil:0.11.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    implementation("androidx.paging:paging-runtime-ktx:3.0.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.security:security-crypto:1.0.0")
+    implementation("com.apollographql.apollo:apollo-runtime:2.5.5")
+    implementation("com.apollographql.apollo:apollo-coroutines-support:2.5.5")
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+}
+apollo {
+    generateKotlinModels.set(true)
+}
---
+++ app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
@@ -0,0 +1,73 @@
+package com.example.rocketreserver
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.apollographql.apollo.api.Input
+import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloException
+import com.example.rocketreserver.databinding.LaunchListFragmentBinding
+import kotlinx.coroutines.channels.Channel
+import java.nio.channels.Channels
+
+class LaunchListFragment : Fragment() {
+    private lateinit var binding: LaunchListFragmentBinding
+    private val launches = mutableListOf<LaunchListQuery.Launch>()
+    val mAdapter = LaunchListAdapter(launches)
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = LaunchListFragmentBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launchWhenResumed {
+
+            binding.launches.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = mAdapter
+            }
+            val channel = Channel<Unit>(Channel.CONFLATED)
+            channel.offer(Unit)
+            mAdapter.onEndOfListReached = {
+                channel.offer(Unit)
+            }
+            var cursor: String? = null
+            for (item in channel){
+                val response = try {
+                    apolloClient(requireContext()).query(LaunchListQuery(cursor = Input.fromNullable(cursor))).await()
+                }catch (e: ApolloException){
+                    Log.d("LaunchList", "Failure", e)
+                    return@launchWhenResumed
+                }
+                Log.d("LaunchList", "Success ${response.data}")
+                val newLaunches = response.data?.launches?.launches?.filterNotNull()
+                if (newLaunches != null){
+                    launches.addAll(newLaunches)
+                    mAdapter.notifyDataSetChanged()
+                }
+                cursor = response.data?.launches?.cursor
+
+                if (response.data?.launches?.hasMore != true){
+                    break
+                }
+
+            }
+
+            mAdapter.onEndOfListReached = null
+            channel.close()
+
+        }
+        mAdapter.onItemClicked = {
+            findNavController().navigate(
+                LaunchListFragmentDirections.openLaunchDetails(launchId = it.id))
+        }
+    }
+}
